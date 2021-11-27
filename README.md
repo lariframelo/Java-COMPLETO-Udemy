a26bbadd47e4ed18b4f446545b2ed9ff963941bd
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-11-27 10:02:10 -0300

    up

--- README.md
+++
@@ -1,2 +0,0 @@
-# Java-COMPLETO-Udemy
- Java COMPLETO Programação Orientada ao Objeto + Projetos. Curso disponibilizado pela South System na plataforma Udemy
--- Tratamento de excessões/src/application/Program.java
+++ Tratamento de excessões/src/application/Program.java
@@ -5,20 +5,25 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+        method2();
+        System.out.println("End of program");
+    }
+
+    public static void method2() {
+        Scanner sc = new Scanner(System.in);
         try {
-            String[] vect = sc.nextLine().split(" ");
+            String [] vect = sc.nextLine().split(" ");
             int position = sc.nextInt();
             System.out.println(vect[position]);
         }
-        catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Invalid Position");
+        catch (ArrayIndexOutOfBoundsException e ) {
+            System.out.println("Invalid position!");
         }
-        catch (InputMismatchException e) {
+        catch (InputMismatchException e ) {
             System.out.println("Input error");
         }
-        System.out.println("End of program");
+
         sc.close();
     }
 }
